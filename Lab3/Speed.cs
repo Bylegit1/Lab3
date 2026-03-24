94e1e695437e0019d90dddd38d87f01d602ef412
Александр Сенченко <senchenko.sashuta@list.ru>
2026-03-24 14:03:47 +0800

    Добавьте файлы проекта.

---
+++ Lab3/Speed.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    public enum MeasureType { mps, kph, kn, max }
+    public class Speed
+    {
+        private double value;
+        private MeasureType type;
+
+        public Speed(double value, MeasureType type)
+        {
+            this.value = value;
+            this.type = type;
+        }
+
+        public string Verbose()
+        {
+            var typeVerbose = "";
+            switch (this.type)
+            {
+                case MeasureType.mps:
+                    typeVerbose = "м/с";
+                    break;
+                case MeasureType.kph:
+                    typeVerbose = "км/ч";
+                    break;
+                case MeasureType.kn:
+                    typeVerbose = "уз";
+                    break;
+                case MeasureType.max:
+                    typeVerbose = "М";
+                    break;
+            }
+            return String.Format("{0} {1}", this.value, typeVerbose);
+        }
+
+        public static Speed operator +(Speed instance, double number)
+        {
+            return new Speed(instance.value + number, instance.type);
+            //var newValue = instance.value + number;
+            //var speed = new Speed(newValue, instance.type);
+            //return speed;
+        }
+
+        public static Speed operator +(double number, Speed instance)
+        {
+            return instance + number;
+        }
+
+        public static Speed operator -(Speed instance, double number)
+        {
+            return new Speed(instance.value - number, instance.type);
+        }
+
+        public static Speed operator -(double number, Speed instance)
+        {
+            return instance - number;
+        }
+
+        public static Speed operator *(Speed instance, double number)
+        {
+            return new Speed(instance.value * number, instance.type);
+        }
+
+        public static Speed operator *(double number, Speed instance)
+        {
+            return instance * number;
+        }
+
+        public Speed To(MeasureType newType)
+        {
+            var newValue = this.value;
+            if (this.type == MeasureType.mps)
+            {
+                switch (newType)
+                {
+                    case MeasureType.mps:
+                        newValue = this.value;
+                        break;
+                    case MeasureType.kph:
+                        newValue = this.value / 0.27;
+                        break;
+                    case MeasureType.kn:
+                        newValue = this.value / 0.514;
+                        break;
+                    case MeasureType.max:
+                        newValue = this.value / 331.46;
+                        break;
+                }
+            }
+            else if (newType == MeasureType.mps)
+            {
+                switch (this.type)
+                {
+                    case MeasureType.mps:
+                        newValue = this.value;
+                        break;
+                    case MeasureType.kph:
+                        newValue = this.value * 3.6;
+                        break;
+                    case MeasureType.kn:
+                        newValue = this.value * 1.94384;
+                        break;
+                    case MeasureType.max:
+                        newValue = this.value * 331.46;
+                        break;
+                }
+            }
+            else
+            {
+                newValue = this.To(MeasureType.mps).To(newType).value;
+            }
+            return new Speed(newValue, newType);
+        }
+
+        public static Speed operator+(Speed speed1, Speed speed2)
+        {
+            return speed1 + speed2.To(speed1.type).value;
+        }
+
+        public static Speed operator -(Speed speed1, Speed speed2)
+        {
+            return speed1 - speed2.To(speed1.type).value;
+        }
+    }
+}
---
+++ Lab3Tests/SpeedTests.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lab3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3.Tests
+{
+    [TestClass()]
+    public class SpeedTests
+    {
+        [TestMethod()]
+        public void VerboseTest()
+        {
+            var speed = new Speed(25, MeasureType.mps);
+            Assert.AreEqual("25 м/с", speed.Verbose());
+
+            speed = new Speed(1, MeasureType.kph);
+            Assert.AreEqual("1 км/ч", speed.Verbose());
+
+            speed = new Speed(2, MeasureType.kn);
+            Assert.AreEqual("2 уз", speed.Verbose());
+
+            speed = new Speed(25, MeasureType.max);
+            Assert.AreEqual("25 М", speed.Verbose());
+        }
+
+        [TestMethod()]
+        public void SummationTest()
+        {
+            var speed = new Speed(5, MeasureType.mps);
+            speed = speed + 2.2;
+            Assert.AreEqual("7,2 м/с", speed.Verbose());
+        }
+
+        [TestMethod()]
+        public void DifferenceTest()
+        {
+            var speed = new Speed(5, MeasureType.kph);
+            speed = speed - 3;
+            Assert.AreEqual("2 км/ч", speed.Verbose());
+        }
+
+        [TestMethod()]
+        public void ProductTest()
+        {
+            var speed = new Speed(5, MeasureType.max);
+            speed = speed * 2;
+            Assert.AreEqual("10 М", speed.Verbose());
+        }
+
+        [TestMethod()]
+        public void MeterToAnyTest()
+        {
+            Speed speed;
+            speed = new Speed(108, MeasureType.mps);
+            Assert.AreEqual("30 км/ч", speed.To(MeasureType.kph).Verbose());
+
+            speed = new Speed(2003, MeasureType.mps);
+            Assert.AreEqual("1030,434603671084 уз", speed.To(MeasureType.kn).Verbose());
+
+            speed = new Speed(2030, MeasureType.mps);
+            Assert.AreEqual("6,124419236106921 М", speed.To(MeasureType.max).Verbose());
+
+            speed = new Speed(1, MeasureType.kph);
+            Assert.AreEqual("3,6 м/с", speed.To(MeasureType.mps).Verbose());
+
+            speed = new Speed(2, MeasureType.kn);
+            Assert.AreEqual("3,88768 м/с", speed.To(MeasureType.mps).Verbose());
+
+            speed = new Speed(3, MeasureType.max);
+            Assert.AreEqual("994,3799999999999 м/с", speed.To(MeasureType.mps).Verbose());
+        }
+       
+        [TestMethod()]
+        public void AddMpsKphKnMax()
+        {
+            var mps = new Speed(10, MeasureType.mps);
+            var kph = new Speed(5, MeasureType.kph);
+            var kn = new Speed(10, MeasureType.kn);
+            var max = new Speed(10, MeasureType.max);
+
+            Assert.AreEqual("28 м/с", (mps + kph).Verbose());
+        }
+
+    }
+}
